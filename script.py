56310b9cccadde03f7d0b5a528c0050b4ef1030e
Tatsuhiro Koshi <tk474@cornell.edu>
2017-10-25 13:43:56 -0400

    gitignore

--- script.py
+++ script.py
@@ -3,33 +3,64 @@ from sklearn.model_selection import cross_val_score
 from sklearn.neural_network import MLPRegressor
 from sklearn import preprocessing
 import numpy as np
+from sklearn.decomposition import PCA
 
-XFILE = 'data/imu_proc10-Oct-2017.csv'
-YFILE = 'data/vicon_proc10-Oct-2017.csv'
-
-
-def svr():
-    X = preprocessing.scale(np.genfromtxt(XFILE, delimiter=','))
-    y = preprocessing.scale(np.genfromtxt(YFILE, delimiter=','))
-    clf = SVR('linear')
-    for i in range(y.shape[1]):
-        scores = cross_val_score(clf, X, y[:, i], cv=10, n_jobs=-1)
-        print(np.mean(scores), np.std(scores))
-    clf = SVR()
-    for i in range(y.shape[1]):
-        scores = cross_val_score(clf, X, y[:, i], cv=10, n_jobs=-1)
-        print(np.mean(scores), np.std(scores))
+XFILE = 'data/imu_proc17-Oct-2017.csv'
+YFILE = 'data/vicon_proc17-Oct-2017.csv'
 
 
 def mlp():
     X = preprocessing.scale(np.genfromtxt(XFILE, delimiter=','))
     y = preprocessing.scale(np.genfromtxt(YFILE, delimiter=','))
-    clf = MLPRegressor(max_iter=500)
+    clf = MLPRegressor(max_iter=500, hidden_layer_sizes=10, alpha=10)
+    print('=== MLP ===')
+    kfold(clf, X, y)
+
+
+def svr_pca():
+    print('=== linear SVR ===')
+    X = preprocessing.scale(np.genfromtxt(XFILE, delimiter=','))
+    y = preprocessing.scale(np.genfromtxt(YFILE, delimiter=','))
+    for n in range(1, X.shape[1]):
+        print(str(n) + ' dimension')
+        pca = PCA(n)
+        X_reduced = pca.fit_transform(X)
+        clf = SVR('linear')
+        kfold(clf, X_reduced, y)
+
+    print('=== RBF SVR ===')
+    for n in range(1, X.shape[1]):
+        print(str(n) + ' dimension')
+        pca = PCA(n)
+        X_reduced = pca.fit_transform(X)
+        clf = SVR()
+        kfold(clf, X_reduced, y)
+
+
+def mlp_pca():
+    X = preprocessing.scale(np.genfromtxt(XFILE, delimiter=','))
+    y = preprocessing.scale(np.genfromtxt(YFILE, delimiter=','))
+
+    print('=== MLP ===')
+    for n in range(1, X.shape[1]):
+        # print(str(n) + ' dimension')
+        pca = PCA(n)
+        X_reduced = pca.fit_transform(X)
+        clf = MLPRegressor(max_iter=500, alpha=10)
+        kfold(clf, X_reduced, y)
+
+
+def kfold(clf, X, y):
+    mean = []
+    std = []
     for i in range(y.shape[1]):
         scores = cross_val_score(clf, X, y[:, i], cv=10, n_jobs=-1)
-        print(np.mean(scores), np.std(scores))
+        mean.append(np.mean(scores))
+        std.append(np.std(scores))
+    print('\t'.join(map(str, mean)))
+    # print('\t'.join(map(str, std)))
 
 
 if __name__ == '__main__':
     mlp()
-    #svr()
+    # svr_pca()
